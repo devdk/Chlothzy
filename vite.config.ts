00aad5f51a3eed149fd16e6c2d48211e3916a3a1
devdk <raodk9122@gmail.com>
2025-07-06 21:49:53 +0530

    vite config u

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "app"),
+    },
+  },
   plugins: [
-    react(),         // Required if using JSX/TSX
+    react(),
     tailwindcss(),
     tsconfigPaths(),
   ],
   server: {
     proxy: {
-      "/api": "http://localhost:5000", // proxy /api/* to backend
+      "/api": "http://localhost:5000",
     },
   },
 });
